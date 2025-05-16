c4526a16d7bbef9e38ae2c32705b622115014e95
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-05-16 16:10:46 +0300

    Add Functionality for the Extension

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let myLeads = []
+
+let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
+let ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
-let myLeads = []
-let inputEl = document.getElementById("input-el")
+function renderLeads() {
+    let listItems = ""
+    for (let l = 0; l < myLeads.length; l++) {
+        listItems += `<li> <a target='_blank' href = ${myLeads[l]}> ${myLeads[l]} </a> </li>`
+    }
+    ulEl.innerHTML = listItems
+}
