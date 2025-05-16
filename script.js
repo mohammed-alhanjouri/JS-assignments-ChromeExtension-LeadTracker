ea7f7acf37bc8be45dc8eee44778785d8292cd84
mohammed-alhanjouri <mohammadalhanjouri@gmail.com>
2025-05-16 20:35:58 +0300

    Create, Style, Function Save Tab Button

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
@@ -19,16 +21,24 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-    // ulEl.textContent = ""
-})
-
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    // ulEl.textContent = ""
 })
